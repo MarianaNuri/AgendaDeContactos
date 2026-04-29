81901b39c6916f5bc3991815da6e5b0cdc88de35
MarianaNuri <L24270017@tuxtla.tecnm.mx>
2026-04-29 12:38:17 -0600

    Cambio de config de la bd

--- config/bd.php
+++ config/bd.php
@@ -1,14 +1,16 @@
 <?php
-$host = "localhost";
-$db = "agenda";
-$user = "root";
-$pass = "";  // XAMPP usa contraseña vacía por defecto
+$host = "sql312.infinityfree.com";
+$db = "if0_41722192_agenda";
+$user = "if0_41722192";
+$pass = "c22sr0TTwX";
 $charset = "utf8mb4";
 
 try {
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $pdo = new PDO($dsn, $user, $pass);
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+    die("Error de conexión");
 }
+?>
